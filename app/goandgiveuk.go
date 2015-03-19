1c9c6f456ded08cc73b2a4cb475b0c62a5af706e
alistair_collins <alistaircollins21@gmail.com>
2015-03-19 21:41:33 +0000

    Stepped back some of the use of template changes, as didn't work on AppEngine.
    Template code didn't work as tested on local, need to think about it
    some more, so just restored it to a more basic state (every web page is
    the index.html)

--- app/goandgiveuk.go
+++ app/goandgiveuk.go
@@ -4,24 +4,37 @@ package goandgiveuk
 import (
 	"net/http"
 
+	//	"appengine"
 	"html/template"
-	//"appengine"
 )
 
 var templates = template.Must(template.ParseFiles(
 	"templates/index.html",
-	))
+	"templates/contact.html",
+	"templates/elements.html",
+	"templates/generic.html",
+))
 
 func init() {
 	http.HandleFunc("/", root)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	//c := appengine.NewContext(r)
+	//	c := appengine.NewContext(r)
+	//	var temp_site string = r.URL.String()
+	//	c.Infof("Requested and truncated URL: %v", temp_site)
+
+	//	if r.URL.String()[:1] == "/" {
+	//		temp_site = temp_site[1:]
+	//	}
+	//	if temp_site == "" {
+	//		temp_site = "index.html"
+	//	}
 
 	err := templates.ExecuteTemplate(w, "index.html", nil)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	//	err := templates.ExecuteTemplate(w, temp_site, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
