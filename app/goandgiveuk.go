dbe419ed6da8d894b6450fe690b0eec580fd1ea6
alistair_collins <alistaircollins21@gmail.com>
2015-03-23 11:26:21 +0000

    Handlers for other pages of the site.

--- app/goandgiveuk.go
+++ app/goandgiveuk.go
@@ -17,6 +17,9 @@ var templates = template.Must(template.ParseFiles(
 
 func init() {
 	http.HandleFunc("/", root)
+	http.HandleFunc("/contact.html", contact)
+	http.HandleFunc("/elements.html", elements)
+	http.HandleFunc("/generic.html", generic)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +36,30 @@ func root(w http.ResponseWriter, r *http.Request) {
 
 	err := templates.ExecuteTemplate(w, "index.html", nil)
 	//	err := templates.ExecuteTemplate(w, temp_site, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "contact.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func elements(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "elements.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func generic(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "generic.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
